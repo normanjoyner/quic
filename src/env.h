d9df0ad40c8a3e9740993b30a1eff384b7741c3d
James M Snell <jasnell@gmail.com>
2019-04-11 15:46:45 -0700

    quic: support SNI
    PR-URL: https://github.com/nodejs/quic/pull/18

--- src/env.h
+++ src/env.h
@@ -291,6 +291,7 @@ constexpr size_t kFsStatsBufferLength = kFsStatsFieldsNumber * 2;
   V(promise_string, "promise")                                                 \
   V(pubkey_string, "pubkey")                                                   \
   V(query_string, "query")                                                     \
+  V(quic_alpn_string, "h3-19")                                                 \
   V(raw_string, "raw")                                                         \
   V(read_host_object_string, "_readHostObject")                                \
   V(readable_string, "readable")                                               \
