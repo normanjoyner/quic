ef3dd41498ea1c6ade4460c579b913e79e07f8ae
James M Snell <jasnell@gmail.com>
2019-05-18 13:40:23 -0700

    quic: emit event on path validation
    Emit a pathValidation event on path validation.
    
    Allows code to handle path validation failures in whatever
    way makes sense for them.
    
    PR-URL: https://github.com/nodejs/quic/pull/29

--- src/env.h
+++ src/env.h
@@ -428,6 +428,7 @@ constexpr size_t kFsStatsBufferLength = kFsStatsFieldsNumber * 2;
   V(quic_on_session_extend_function, v8::Function)                             \
   V(quic_on_session_handshake_function, v8::Function)                          \
   V(quic_on_session_ticket_function, v8::Function)                             \
+  V(quic_on_session_path_validation_function, v8::Function)                    \
   V(quic_on_stream_ready_function, v8::Function)                               \
   V(quic_on_stream_close_function, v8::Function)                               \
   V(quic_on_stream_error_function, v8::Function)                               \
